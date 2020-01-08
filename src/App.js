9e666c3f24b6a13520b2268841b559e9b17dcd79
u88803494 <u88803494@gmail.com>
2020-01-08 20:45:49 +0800

    繼承 w22h1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { HashRouter as Router, Route } from "react-router-dom";
+import Nav from './nav/';
+import Home from './home/';
+import PostList from './post_list/';
+import Posts from './posts/';
 
-function App() {
+function About() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="about">
+      <h1 className="about__title">maxime quas veniam</h1>
+      <div className="about__content">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Omnis alias harum voluptatem nostrum vero mollitia cum,
+        voluptas neque praesentium provident quasi, obcaecati enim consequatur illo.
+        Hic autem minus aperiam velit.
+        Ducimus corrupti iusto officia aperiam eius ad neque sit minima ut
+        nostrum aliquideaque qui maxime quas veniam doloremque quaerat repellendus esse,
+        rem dolorum tempora perspiciatis impedit?
+        Possimus omnis pariatur et quia eum molestiae, sint unde,
+        reprehenderit recusandae consequuntur iusto eos quis ipsum veritatis,
+        tempore deleniti totam sunt nisi a!
+        </div>
     </div>
-  );
+  )
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Nav />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/posts" component={PostList} />
+          <Route path="/about" component={About} />
+          <Route path="/posts/:postId" component={Posts} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
