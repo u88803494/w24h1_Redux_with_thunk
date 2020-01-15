be3cb460642bd49473430cd7f00c2b91fc101a8e
Hugh <u88803494@gmail.com>
2020-01-15 09:37:40 +0800

    修改到一半

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Nav.css';
 import { Link, Route } from "react-router-dom";
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
+
 
 const Item = ({ to, text, exact }) => (
   <Route
@@ -16,7 +18,33 @@ const Item = ({ to, text, exact }) => (
   />
 );
 
-const Nav = ({ page, onClick }) => (
+const TheNavbar = ({ page, onClick }) => (
+  <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
+    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="#posts">Posts</Nav.Link>
+        <Nav.Link href="#about">About</Nav.Link>
+        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+      <Nav>
+        <Nav.Link href="#deets">More deets</Nav.Link>
+        <Nav.Link eventKey={2} href="#memes">
+          Dank memes
+      </Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+);
+
+const Nav1 = ({ page, onClick }) => (
   <nav className="nav">
     <header><h1>BlOG</h1></header>
     <Item to='/' exact={true} text='首頁' />
@@ -26,4 +54,4 @@ const Nav = ({ page, onClick }) => (
 );
 
 
-export default Nav;
+export default TheNavbar;
