f1304a6a8954f872eaec2bbab678eae1632d602e
Hugh <u88803494@gmail.com>
2020-02-19 22:32:17 +0800

    補上一些想法

--- src/WebAPI.js
+++ src/WebAPI.js
@@ -14,3 +14,8 @@ export const updatePost = post =>
 
 export const deletePost = postId =>
   axios.delete('https://qootest.com/posts/' + postId)
+
+/**
+ * 當需要展示的時候，可以加上錯誤的 API，並加上按鈕。
+ * 跟實際有作用的按鈕分開，這樣就可以很方便的展示。
+ */
