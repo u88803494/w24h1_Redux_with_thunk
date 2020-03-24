003ef27bd6a46f164d3387378cead2c2802a82cf
Hugh <u88803494@gmail.com>
2020-03-24 13:41:01 +0800

    Updates

---
+++ precache-manifest.c660aee483eb216d5ff12d5de0ab899e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37cbf6452fb14f60a416d3b282368901",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9e022fd8b51b37cfef4f",
+    "url": "./static/css/main.6484b2fd.chunk.css"
+  },
+  {
+    "revision": "5cd9c344d37195673ae7",
+    "url": "./static/js/2.aaa444df.chunk.js"
+  },
+  {
+    "revision": "2b0a374b6ddc7380ebd20b5fd59e60e9",
+    "url": "./static/js/2.aaa444df.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9e022fd8b51b37cfef4f",
+    "url": "./static/js/main.c24d5652.chunk.js"
+  },
+  {
+    "revision": "37351db21f5dd14502c4",
+    "url": "./static/js/runtime-main.16cc8184.js"
+  }
+]);
